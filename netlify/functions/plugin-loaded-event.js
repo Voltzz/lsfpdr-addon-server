f1c16bb1396642c7254b60b253213d770438d6a2
voltzz <voltz@ZENITH>
2022-11-23 23:45:30 -0500

    Add plugin loaded event

---
+++ netlify/functions/plugin-loaded-event.js
@@ -0,0 +1,28 @@
+import { PostHog } from 'posthog-node'
+
+export const handler = async (event, context) => {
+    const body = JSON.parse(event.body)
+    console.log("[Plugin Loaded] Request received")
+
+    const client = new PostHog(
+        'phc_Ae20eH6ZukSpndSBBaxx6odHM5HDZYmguBRXA0Fo5da',
+        { host: 'https://app.posthog.com' }
+    )
+
+    client.capture({
+        distinctId: body.userId,
+        event: 'PluginLoaded',
+        difficulty: body.difficulty,
+        version: body.version,
+        dynamicWorld: body.dynamicWorld,
+        persistDifficulty: body.persistDifficulty,
+    })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+}
