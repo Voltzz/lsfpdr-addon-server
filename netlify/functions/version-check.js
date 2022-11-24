30aa5272c7e6319cf3365387ddc0ba795cca97b8
voltzz <voltz@ZENITH>
2022-11-23 21:48:00 -0500

    Add logging

--- netlify/functions/version-check.js
+++ netlify/functions/version-check.js
@@ -3,6 +3,7 @@ export const handler = async (event, context) => {
     const body = JSON.parse(event.body)
     const currVer = body.version
     const updatedRequired = !(latestVer == currVer)
+    console.log("[Version Check] Request received, responded with: " + updatedRequired)
     return {
         statusCode: 200,
         body: JSON.stringify({
