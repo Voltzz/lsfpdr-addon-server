539f79e8a28593be6bab7b5b0a2263cfc18e0ae2
voltzz <voltz@ZENITH>
2022-11-23 19:44:55 -0500

    Setup version check API

---
+++ netlify/functions/version-check.js
@@ -0,0 +1,12 @@
+export const handler = async (event, context) => {
+    const latestVer = "1.0.0.0"
+    const body = JSON.parse(event.body)
+    const currVer = body.version
+    const updatedRequired = !(latestVer == currVer)
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            updateRequired: updatedRequired,
+        }),
+    }
+}
